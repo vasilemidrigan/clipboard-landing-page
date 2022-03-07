4608574bb2a82051262663d7cad9efcaf46d9242
Vasile Midrigan <111rigan111@gmail.com>
2022-03-07 04:20:02 +0000

    fixing tablet positioning and some other stuff

--- script.js
+++ script.js
@@ -2,16 +2,18 @@
 
 // Variables
 const imgDesk = document.querySelector(".desk");
+const tabletImg = document.querySelector(".image-tablet");
 const mainA = document.querySelector(".main__a");
-const deskImage = document.querySelector(".image");
+const deskImage = document.querySelector(".grid-image");
 const cntntDefault = document.querySelectorAll(".cntnt-default");
-console.log("cntntDefault:", cntntDefault);
 
+// Creating the grid for desktop image and 3 items
+//   if screen width is greather than 1000px
 if (window.innerWidth > 1000) {
   const grid = document.createElement("div");
   grid.className = "grid";
 
-  mainA.appendChild(grid);
+  mainA.insertBefore(grid, mainA.children[5]);
 
   grid.appendChild(deskImage);
   deskImage.classList.add("img-grid");
