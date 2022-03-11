e95290224ca56d7c567fab6ad94f82b2e8221b41
Vasile Midrigan <111rigan111@gmail.com>
2022-03-11 05:09:01 +0000

    refactoring and fixing row logo icons

--- script.js
+++ script.js
@@ -4,17 +4,27 @@
 const imgDesk = document.querySelector(".desk");
 const tabletImg = document.querySelector(".image-tablet");
 const mainA = document.querySelector(".main__a");
+const mainB = document.querySelector(".main__b");
+const mainC = document.querySelector(".main__c");
 const deskImage = document.querySelector(".grid-image");
 const cntntDefault = document.querySelectorAll(".cntnt-default");
+const cntntDefaultItem = document.querySelectorAll(".cntnt-default__item");
 
-// Creating the grid for desktop image and 3 items
-//   if screen width is greather than 1000px
 if (window.innerWidth > 1000) {
+  // main__b
+  const toFlex = document.createElement("div");
+  toFlex.className = "toFlex";
+  mainB.insertBefore(toFlex, mainB.children[1]);
+  for (let i = 0; i < cntntDefaultItem.length; i++) {
+    toFlex.appendChild(cntntDefaultItem[i]);
+  }
+  // main__c
+  mainC.children[0].classList.add("toFlex");
+
+  // grid
   const grid = document.createElement("div");
   grid.className = "grid";
-
   mainA.insertBefore(grid, mainA.children[5]);
-
   grid.appendChild(deskImage);
   deskImage.classList.add("img-grid");
 
